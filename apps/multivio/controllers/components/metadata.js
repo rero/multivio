2eff4e7f3b4b6b51b390dae1c56e54cac9e256d9
hec <christelle.heritier@rero.ch>
2010-05-18 12:20:31 +0200

    Removed metadataController bindings

--- apps/multivio/controllers/components/metadata.js
+++ apps/multivio/controllers/components/metadata.js
@@ -19,36 +19,20 @@
 
 Multivio.metadataController = SC.ObjectController.create(
 /** @scope Multivio.metadataController.prototype */ {
-
-  /**
-    Binds to the cdm metadata
-
-    @binding {hash}
-   */
-  metadata: null,
-  metadataBinding: SC.Binding.oneWay("Multivio.CDM.metadata"),
   
   /**
-    Initialize this controller by setting its content if its 
+    Initialize this controller by setting its content 
 
     @param {String} url the current file url
   */
   initialize: function (url) {
     var meta = Multivio.CDM.getMetadata(url);
     if (!SC.none(meta) && meta !== -1) {
+      console.info('MEtadata set content');
       this.set('content', meta);
     }
     Multivio.logger.info('metadataController initialized');    
   },
-  
-  metadataDidChange: function () {
-    var cf = Multivio.masterController.get('currentFile');
-    var meta = this.get('metadata')[cf];
-    if (!SC.none(meta) && meta !== -1) {
-      this.set('content', meta);
-    }
-  }.observes('metadata'), 
-  
    
   /**
     The document's descriptive metadata
