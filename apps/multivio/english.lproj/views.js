6328526823f2506a0d1500337d2a6a059e17d562
mmo <mm@jade.local>
2010-11-10 23:51:07 +0100

    Interface adjustments
    * Prepare button for showing toolbar
    
    * Solve alpha transparency problem in IE8

--- apps/multivio/english.lproj/views.js
+++ apps/multivio/english.lproj/views.js
@@ -375,6 +375,20 @@ Multivio.views = SC.Page.design(
           target: "Multivio.paletteController",
           action: "showHorizontalToolbar"
         })
+        /*,
+        SC.ButtonView.design({
+          layout: { bottom: 24, centerX: 0, width: 32, height: 32 },
+          titleMinWidth : 0,
+          needsEllipsis: NO,
+          name: 'show_toolbar',
+          toolTip: '_ShowToolbar'.loc(),
+          renderStyle: "renderImage",
+          icon: 'show_toolbar_new',
+          theme: 'mvo-button',
+          target: "Multivio.paletteController",
+          action: "showHorizontalToolbar"
+        })
+        */
       ]
     }),
 
