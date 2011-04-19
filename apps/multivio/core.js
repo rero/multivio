849bf186b2da864533f534bc3e9847c9a2dd1da6
mmo <moreiram@ginkgo.local>
2011-04-19 11:46:28 +0200

    Version 20110419 for test deployment

--- apps/multivio/core.js
+++ apps/multivio/core.js
@@ -17,7 +17,7 @@ Multivio = SC.Application.create(
   /** @scope Multivio.prototype */ {
 
   NAMESPACE: 'Multivio',
-  VERSION: '0.4.0',
+  VERSION: '20110419',
 
   // This is your application store.  You will use this store to access all
   // of your model data.  You can also set a data source on this store to
